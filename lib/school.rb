488e60a87a87d6db315208c6947be12872179c08
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-12 08:21:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,7 @@
 class School
-  def initialize(name)
-    @name = name
+
+  def initialize(school_name)
+    @school_name = school_name
     @roster = {}
   end
 
@@ -9,19 +10,21 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster[grade] != nil
-      @roster[grade] << name
-    else
-      @roster[grade] = [name]
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
     end
+    @roster[grade] << name
   end
 
-  def grade(grade)
-    @roster[grade]
+  def grade(number)
+    @roster[number]
   end
 
   def sort
-    @roster.each do |key, value|
-      value.sort!
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
     end
   end
+end
