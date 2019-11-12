051141b1012c5bceeb2d4630864a11e9b17d3c80
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-12 08:19:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,5 @@
-# code here!
-school = School.new("Bayside High School")
-school.add_student("Zach Morris", 9)
-school.roster
-hash["new_key"] = []
-hash["new_key"] << "new_value_for_value_array"
-
-hash
- => {"new_key"=>["new_value_for_value_array"]} 
+class School
+  def initialized(name)
+    @name = name
+    @roster = {}
+  end  
